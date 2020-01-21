c37ec61631dd0384f2a5bbffeb5291331c1b00f3
Willem Liu <willem_liu@hotmail.com>
2020-01-21 14:20:03 +0100

    Experimental SPA support

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { Content } from "./Content";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { lightOrDark } from "../utils/lightOrDark";
 
 declare var chrome: any;
 declare var browser: any;
@@ -8,10 +9,51 @@ declare var browser: any;
 const runtime =
     typeof browser !== "undefined" ? browser.runtime : chrome.runtime;
 
+function getCanonical() {
+    const canonical = document.querySelector('link[rel="canonical"]');
+    return canonical
+        ? canonical.getAttribute("href") || "about:blank"
+        : window.location.origin +
+              (window.location.pathname.length > 1
+                  ? window.location.pathname
+                  : "");
+}
+
 function App(props: any) {
+    const [canonical, setCanonical] = useState("");
+    const [themeColor, setThemeColor] = useState("#fff");
     const [opened, setOpened] = useState(props.opened || false);
 
     useEffect(() => {
+        const observer = new MutationObserver((mutationsList, observer) => {
+            if (canonical && getCanonical()) {
+                console.log(
+                    "URL changed?",
+                    canonical != getCanonical(),
+                    "canonical: ",
+                    canonical,
+                    "getCanonical: ",
+                    getCanonical()
+                );
+                setCanonical(getCanonical());
+            }
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+    }, [canonical]);
+
+    useEffect(() => {
+        let theme = "#fff";
+        const themeMeta = document.querySelector('meta[name="theme-color"]');
+        if (themeMeta?.getAttribute("content")) {
+            theme = themeMeta.getAttribute("content") || "#fff";
+            if (lightOrDark(theme) === "dark") {
+                theme = "#fff";
+            }
+        }
+        setThemeColor(theme);
+
+        setCanonical(getCanonical());
+
         runtime.onMessage.addListener(
             (request: any, sender: any, sendResponse: any) => {
                 setOpened((open: boolean) => !open);
@@ -28,17 +70,17 @@ function App(props: any) {
         <>
             <Content
                 url={`https://universal-comments.willemliu.now.sh/api/count?canonical=${encodeURI(
-                    props.canonical
+                    canonical
                 )}`}
                 open={opened}
                 onCollapsed={onCollapsed}
                 hideButtonOverlay={props.hideButtonOverlay}
-                themeColor={props.themeColor}
+                themeColor={themeColor}
                 overlayHeight={props.overlayHeight}
             >
                 <StyledIframe
                     src={`https://universal-comments.willemliu.now.sh?canonical=${encodeURI(
-                        props.canonical
+                        canonical
                     )}`}
                 />
             </Content>
--- src/content.tsx
+++ src/content.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createGlobalStyle } from "styled-components";
 import { App } from "./components/App";
-import { lightOrDark } from "./utils/lightOrDark";
 
 declare var chrome: any;
 declare var browser: any;
@@ -28,36 +27,14 @@ storage.sync.get(
         console.log("Universal Comments enabled", items.enable);
 
         if (items.enable) {
-            // Render the read history widget
             const rootEl = document.createElement("section");
             rootEl.setAttribute("class", "universal-comments-extension");
             document.body.appendChild(rootEl);
-
-            const canonical = document.querySelector('link[rel="canonical"]');
-            const url = canonical
-                ? canonical.getAttribute("href") || "about:blank"
-                : window.location.origin +
-                  (window.location.pathname.length > 1
-                      ? window.location.pathname
-                      : "");
-
-            let themeColor = "#fff";
-            const themeMeta = document.querySelector(
-                'meta[name="theme-color"]'
-            );
-            if (themeMeta?.getAttribute("content")) {
-                themeColor = themeMeta.getAttribute("content") || "#fff";
-                if (lightOrDark(themeColor) === "dark") {
-                    themeColor = "#fff";
-                }
-            }
 
             ReactDOM.render(
                 <>
                     <GlobalStyle />
                     <App
-                        themeColor={themeColor}
-                        canonical={url}
                         hideButtonOverlay={items.hideButtonOverlay}
                         overlayHeight={items.overlayHeight}
                     />
