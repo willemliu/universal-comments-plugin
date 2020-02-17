3a2be33d41969c5ae9917d27e5e0197532b5861a
Willem Liu <willem_liu@hotmail.com>
2020-02-17 18:06:13 +0100

    Added icon badge

--- universal-comments/background.js
+++ universal-comments/background.js
@@ -7,6 +7,34 @@ const tabs = typeof browser !== "undefined" ? browser.tabs : chrome.tabs;
 
 browserAction.onClicked.addListener(function(tab) {
     tabs.sendMessage(tab.id, { greeting: "hello" }, function(response) {
-        console.log(JSON.stringify(response));
+        console.log(response);
     });
 });
+
+tabs.onActivated.addListener(function(activeTab) {
+    getCount(activeTab.tabId);
+});
+
+tabs.onUpdated.addListener(getCount);
+
+function getCount(tabId) {
+    tabs.get(tabId, function(tab) {
+        fetch(
+            `https://universal-comments.willemliu.now.sh/api/count?canonical=${encodeURI(
+                tab.url.replace(/\/$/, "")
+            )}`,
+            {
+                headers: {
+                    "Cache-Control": "no-cache"
+                }
+            }
+        )
+            .then((res) => res.json())
+            .then((json) => {
+                const value = json.count ? json.count : "";
+                browserAction.setBadgeText({
+                    text: `${value}`
+                });
+            });
+    });
+}
